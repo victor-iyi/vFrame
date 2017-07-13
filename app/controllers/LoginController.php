73131811cd13f027bfa29be65650c6c3e6f66654
victor-iyiola <javafolabi@gmail.com>
2017-07-13 19:15:58 +0100

    LoginModel created
    The LoginModel is responsible for fetching data from the database or interacting with other classes. 
    Basically, the logic for the login goes in the model or external abstracted classes for flexibility (Single Responsibility Principle)

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -16,19 +16,36 @@ use App\Core\Controller;
 class LoginController extends Controller
 {
 
+  /**
+   * LoginController constructor.
+   */
   public function __construct()
   {
-    parent::__construct();
-    $this->view->title = "Login";
+    parent::__construct();  // this is necessary to be able to gain access to the View class
   }
 
   /**
    * Default method run by every controller.
+   * @endpoint  /vFrame/login (or /vFrame/login/index)
    * @return mixed
    */
   public function index()
   {
-    $this->view->render("login/index");
+    $this->view->title = "Login | Users"; // sets the page title
+    $this->view->render("login/index"); // loads the login/index view
   }
+
+
+  /**
+   * admin login section
+   * @endpoint /vFrame/login/admin
+   * @credit: Victor I. Afolabi
+   */
+  public function admin()
+  {
+    $this->view->title = "Login | Admin"; // sets the page title
+    $this->view->render("login/admin"); // loads in the login/admin view
+  }
+
 
 }
--- app/models/LoginModel.php
+++ app/models/LoginModel.php
@@ -11,7 +11,39 @@
 namespace App\Models;
 
 
-class LoginModel
+use App\Core\Model;
+
+class LoginModel extends Model
 {
+
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  /**
+   * Handles login for the users
+   * @credit: Victor I. Afolabi
+   */
+  public function userLogin()
+  {
+    // logic for login the admin in goes here...
+    // but to enable proper abstraction.
+    // Why don't we create a separate User class
+    // to handle all the activities of the user
+  }
+
+  /**
+   * Handles login for the admins
+   * @credit: Victor I. Afolabi
+   */
+  public function adminLogin()
+  {
+    // logic for login the admin in goes here...
+    // but to enable proper abstraction.
+    // Why don't we create a separate Admin class
+    // to handle all the activities of the admin ( Single Responsibility Principle)
+
+  }
 
 }
