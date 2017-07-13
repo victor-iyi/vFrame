afef8e71d1fdd1274d3f6f1eeddca22efab78541
victor-iyiola <javafolabi@gmail.com>
2017-07-13 19:31:39 +0100

    Single Responsibility Principle
    Abstracted classes to handle admin and regular users privately

---
+++ app/libs/classes/Admin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author
+ * Created by victor.
+ * A.I. engineer & Software developer
+ * javafolabi@gmail.com
+ * On 13 07, 2017 @ 7:28 PM
+ * Copyright victor © 2017. All rights reserved.
+ */
+
+namespace App\Libs\Classes;
+
+
+class Admin
+{
+
+}
---
+++ app/libs/classes/User.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @author
+ * Created by victor.
+ * A.I. engineer & Software developer
+ * javafolabi@gmail.com
+ * On 13 07, 2017 @ 7:16 PM
+ * Copyright victor © 2017. All rights reserved.
+ */
+
+namespace App\Libs\Classes;
+
+
+use App\Core\Classes;
+
+class User extends Classes
+{
+
+  /**
+   * User constructor.
+   */
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+
+  /**
+   * user login
+   * @credit: Victor I. Afolabi
+   */
+  public function login()
+  {
+    // login logic
+  }
+
+  /**
+   * user logout
+   * @credit: Victor I. Afolabi
+   */
+  public function logout()
+  {
+
+  }
+
+//  /**
+//   * Validates the existence of a user
+//   * @credit: Victor I. Afolabi
+//   */
+//  private function _userExists()
+//  {
+//
+//  }
+}
