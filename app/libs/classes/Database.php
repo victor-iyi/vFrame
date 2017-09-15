5cd8890fabb97f991d79d4d8d18450cfcbdcaf35
victor-iyiola <javafolabi@gmail.com>
2017-09-15 15:15:15 +0100

    Update vFrame MVC Framework

--- app/libs/classes/Database.php
+++ app/libs/classes/Database.php
@@ -14,10 +14,11 @@ use App\Controllers\_ErrorController;
 
 class Database
 {
+  # !- Functions are organized with the CRUD format
 
   private $conn;
-  public $success;
   private $status;
+  public $success;
 
   /**
    * Database constructor.
@@ -70,21 +71,17 @@ class Database
   }
 
   /**
-   * Updates a row in a table
+   * Returns columns matching specified criteria
+   *
    * @param $table
-   * @param $id
-   * @param $bindings
+   * @param $field
+   * @param $value
    * @return bool|\PDOStatement
+   * @internal param $id
    */
-  public function update($table, $bindings, $field, $record)
+  public function select($table, $field, $value)
   {
-    $structure = "";
-    foreach ($bindings as $key => $value)
-      $structure .= $key . "=:" . $key . ", ";
-    $structure = rtrim($structure, ", ");
-    $bindings[$field] = $record;
-    $result = $this->query("UPDATE " . $table . " SET {$structure} WHERE {$field} = :{$field}", $bindings);
-    return $result;
+    return $this->query("SELECT * FROM " . $table . "WHERE {$field} = :${field}", [$field => $value]);
   }
 
   /**
@@ -98,6 +95,90 @@ class Database
     if ($limit) $result = $this->query("SELECT * FROM {$table} LIMIT {$limit}");
     else $result = $this->query("SELECT * FROM {$table}");
     return ($result && $result->rowCount() > 0) ? $result->fetchAll() : [];
+  }
+
+  /**
+   * Returns all fields where primary key matches foreign key
+   *
+   * @credits Victor I. Afolabi
+   * @param $table1 string The first table
+   * @param $table2 string The second table
+   * @param $pk string The primary key field
+   * @param $fk string The foreign key field
+   * @return bool|\PDOStatement
+   */
+  public function innerJoinAll($table1, $table2, $pk, $fk)
+  {
+    return $this->query("SELECT * FROM {$table1} INNER JOIN {$table2} ON $pk = $fk");
+  }
+
+  /**
+   * Inner join with WHERE clause
+   *
+   * @credits Victor I. Afolabi
+   * @param $table1 string The first table
+   * @param $table2 string The second table
+   * @param $pk string the primary key
+   * @param $fk string the foreign key
+   * @param $field string specific field to select from
+   * @param $value string value of the field
+   * @return bool|\PDOStatement
+   */
+  public function innerJoin($table1, $table2, $pk, $fk, $field, $value)
+  {
+    return $this->query("SELECT * FROM `{$table1}` INNER JOIN `{$table2}` ON `{$table1}`.`$pk` = `$fk` WHERE $field = :{$field}",
+      [$field => $value]);
+  }
+
+  /**
+   * Determines if specified entry exists
+   * @credit Banjo Mofesola Paul
+   * @param  string $value The value to search for
+   * @param  string $column The column to search under
+   * @param  string $table The table to search in
+   * @return bool
+   */
+  public function exists($value, $column, $table)
+  {
+    $q = $this->query("SELECT 1 FROM `$table` WHERE `$column` = :value LIMIT 1", ["value" => $value]);
+    return (bool)$q->fetchColumn();
+  }
+
+  /**
+   * Returns a specific column from a table, given another column value
+   *
+   * @credit Banjo Mofesola Paul
+   * @param string $table The table to search in
+   * @param string $dis The target column whose value we're getting
+   * @param string $that The known column
+   * @param string $value Value of known column
+   */
+  public function thisFromThat($table, $dis, $that, $value)
+  {
+    $sql = "SELECT `$dis` FROM `$table` WHERE `$that` = :value LIMIT 1";
+
+    $q = $this->query($sql, ['value' => $value]);
+    if ($o = $q->fetchObject()) return $o->$dis;
+  }
+
+  /**
+   * Updates a row in a table
+   * @param $table
+   * @param $bindings
+   * @param $field
+   * @param $record
+   * @return bool|\PDOStatement
+   * @internal param $id
+   */
+  public function update($table, $bindings, $field, $record)
+  {
+    $structure = "";
+    foreach ($bindings as $key => $value)
+      $structure .= $key . "=:" . $key . ", ";
+    $structure = rtrim($structure, ", ");
+    $bindings[$field] = $record;
+    $result = $this->query("UPDATE " . $table . " SET {$structure} WHERE {$field} = :{$field}", $bindings);
+    return $result;
   }
 
   /**
@@ -122,17 +203,6 @@ class Database
   }
 
   /**
-   * Returns all columns form a row in a table
-   * @param $table
-   * @param $id
-   * @return bool|\PDOStatement
-   * */
-  public function selectOne($table, $id)
-  {
-    return $this->query("SELECT * FROM " . $table . "WHERE id=:id", ["id" => $id]);
-  }
-
-  /**
    * Runs an SQL command to DROP a table
    * @param $table
    * @return bool|\PDOStatement
@@ -149,36 +219,6 @@ class Database
   public function getStatusMessage()
   {
     return $this->success ? "Connection successful!" : $this->status;
-  }
-
-  /**
-   * Determines if specified entry exists
-   * @credit Banjo Mofesola Paul
-   * @param  string $value The value to search for
-   * @param  string $column The column to search under
-   * @param  string $table The table to search in
-   * @return bool
-   */
-  public function exists($value, $column, $table)
-  {
-    $q = $this->query("SELECT 1 FROM `$table` WHERE `$column` = :value LIMIT 1", ["value" => $value]);
-    return (bool)$q->fetchColumn();
-  }
-
-  /**
-   * Returns a specific column from a table, given another column value
-   * @credit Banjo Mofesola Paul
-   * @param string $table The table to search in
-   * @param string $dis The target column whose value we're getting
-   * @param string $that The known column
-   * @param string $value Value of known column
-   */
-  public function thisFromThat($table, $dis, $that, $value)
-  {
-    $sql = "SELECT `$dis` FROM `$table` WHERE `$that` = :value LIMIT 1";
-
-    $q = $this->query($sql, ['value' => $value]);
-    if ($o = $q->fetchObject()) return $o->$dis;
   }
 
 }
